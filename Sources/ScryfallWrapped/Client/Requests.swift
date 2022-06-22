41191a09dbb78a374586b6442324dae9f6045f3a
Zaheer <ZMoola@jhb.dvt.co.za>
2022-06-22 17:18:18 +0200

    Made function static for testing

--- Sources/ScryfallWrapped/Client/Requests.swift
+++ Sources/ScryfallWrapped/Client/Requests.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 public struct Sets {
-    public func getAllSets(completionHandler: @escaping (Result<ScryfallList<MagicSet>, Error>) -> Void) {
+    public static func getAllSets(completionHandler: @escaping (Result<ScryfallList<MagicSet>, Error>) -> Void) {
         URLSession.shared.endpointRequest(.allSets,
                                           expectedType: ScryfallList<MagicSet>.self,
                                           then: completionHandler)
